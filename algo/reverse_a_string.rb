c225156805e17755ce30f337aed65eac4d0af05a
KisleK <kisle.kuhn1@gmail.com>
2017-04-29 21:56:46 -0500

    weekend_work update

--- algo/reverse_a_string.rb
+++ algo/reverse_a_string.rb
@@ -1,10 +1,22 @@
+
 # Complete the method called reverse_a_string that accepts a string as a parameter and 
 # returns the reverse. The one caveat: Don't use the reverse method that already
 # comes with Ruby!
 
 def reverse_a_string(string)
 
+  array = string.split(//)
+  i = 1
+  l = array.length
+  backwards = []
+  l.times do 
+    backwards << array[l - i] 
+    i += 1
+  end
+
+  p backwards.join
 end
+
 
 # Driver code - don't touch anything below this line.
 puts "TESTING reverse_a_string..."
